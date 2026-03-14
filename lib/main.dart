686ea903afb5ff7c9b62427f99b00d5c4b3f305a
Al-Fuad <smalfuadnur@gmail.com>
2026-03-14 19:44:39 +0600

    feat: Add initial binding to MainApp for dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:test_project/core/binding/initial_binding.dart';
 import 'package:test_project/core/routes/app_pages.dart';
 import 'package:test_project/core/routes/app_routes.dart';
 import 'package:test_project/core/themes/app_theme.dart';
@@ -19,6 +20,7 @@ class MainApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
+      initialBinding: InitialBinding(),
       initialRoute: AppRoutes.splash,
       getPages: AppPages.pages,
     );
